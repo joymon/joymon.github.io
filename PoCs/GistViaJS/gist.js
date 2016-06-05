72d5d3e4cdc7bebdc49c43815daa4e70c2c7e8f6
Joy George Kunjikkuru <joymon@gmail.com>
2016-06-04 22:19:49 -0400

    Update gist.js

--- PoCs/GistViaJS/gist.js
+++ PoCs/GistViaJS/gist.js
@@ -1,5 +1,5 @@
 function displayGistCount(){
-  var github = new Github({} ); 3
+  var github = new GitHub({} ); 3
   var repo = github.getRepo( "gollum", "gollum" ); 4
   repo.show( function(err, repo) { 5
     alert(repo);
