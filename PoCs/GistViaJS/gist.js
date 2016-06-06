07a21e9639c12fd412cbaaeeba6efcdbdeb317d4
Joy George Kunjikkuru <joymon@gmail.com>
2016-06-06 14:09:11 -0400

    Update gist.js

--- PoCs/GistViaJS/gist.js
+++ PoCs/GistViaJS/gist.js
@@ -8,11 +8,35 @@ function displayGistCount(){
 }
 
 function addGist(){
-  if(firebase.auth().currentUser){
-    alert("Token is " + document.getElementById('quickstart-oauthtoken').textContent);
-    alert(firebase.auth().currentUser);
+  var token=document.getElementById('quickstart-oauthtoken').textContent;
+  if(firebase.auth().currentUser && token){
+    addHardcodedGistToLoggedInUser(token);
+    //alert("Token is " + token);
+    //alert(firebase.auth().currentUser);
   }
   else{
     alert("firebase.auth().currentUser is not set");
   }
+}
+function addHardcodedGistToLoggedInUser(authToken) {
+  var github = new GitHub({token:authToken});
+  var repo = github.getRepo( "joymon", "joymon" );
+  var gist= github.getGist();
+  gist.create({
+    "description": "Test coded public gist",
+    "public": true,
+    "files": {
+      "file1.karel": {
+        "content": "A sample karel progrma!"
+      }
+    }
+  }).then(function(data) {
+    // Promises!
+    let gistJson = data;
+    gist.read(function(err, gist, xhr) {
+      if(err===null){
+        alert(gist);
+      }
+    });
+  });
 }
