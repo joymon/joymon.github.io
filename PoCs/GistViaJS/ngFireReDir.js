8dc1ac49e643a0dcf4ddea8a4d684ec28e35c594
Joy George Kunjikkuru <joymon@gmail.com>
2016-06-17 23:46:01 -0400

    Update ngFireReDir.js

--- PoCs/GistViaJS/ngFireReDir.js
+++ PoCs/GistViaJS/ngFireReDir.js
@@ -1,4 +1,7 @@
-var app=angular.module('ngFireApp',[]);
-app.controller('ngFireReDirController', ['$scope', function($scope) {
+var app=angular.module('ngFireApp',['firebase']);
+app.controller('ngFireReDirController', ['$scope', '$firebase',function($scope,$firebase) {
   $scope.Test = "Testing success";
+  $scope.init=function(){
+    var ref = new Firebase('project-5197086768869881638.firebaseapp.com'); 
+  };
 }]);
