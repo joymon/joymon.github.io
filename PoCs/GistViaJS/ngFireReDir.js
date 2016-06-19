e41af3099312b6ae0200630287fd3902dc6093af
Joy George Kunjikkuru <joymon@gmail.com>
2016-06-19 13:35:56 -0400

    Update ngFireReDir.js

--- PoCs/GistViaJS/ngFireReDir.js
+++ PoCs/GistViaJS/ngFireReDir.js
@@ -1,7 +1,15 @@
 var app=angular.module('ngFireApp',['firebase']);
-app.controller('ngFireReDirController', ['$scope', '$firebase',function($scope,$firebase) {
+app.controller('ngFireReDirController', ['$scope', '$firebase','$firebaseAuth',function($scope,$firebase,$firebaseAuth) {
   $scope.Test = "Testing success";
   $scope.init=function(){
-    var ref = new Firebase('project-5197086768869881638.firebaseapp.com'); 
+    var ref = new Firebase('project-5197086768869881638.firebaseapp.com');
+     // create an instance of the authentication service
+    var auth = $firebaseAuth(ref);
+    // login with Facebook
+    auth.$authWithOAuthPopup("github").then(function(authData) {
+      console.log("Logged in as:", authData.uid);
+    }).catch(function(error) {
+      console.log("Authentication failed:", error);
+    });
   };
 }]);
